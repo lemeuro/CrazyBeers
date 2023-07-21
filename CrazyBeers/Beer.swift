976a1099d66f47b89f331fd1c7bc88b057671ab7
Lem Euro <mr.lemeuro@gmail.com>
2023-07-21 17:55:44 +0100

    Finish app

---
+++ CrazyBeers/Beer.swift
@@ -0,0 +1,8 @@
+//
+//  Beer.swift
+//  CrazyBeers
+//
+//  Created by Lem Euro on 21/07/2023.
+//
+
+import Foundation
---
+++ CrazyBeers/PunkAPI.swift
@@ -0,0 +1,8 @@
+//
+//  PunkAPI.swift
+//  CrazyBeers
+//
+//  Created by Lem Euro on 21/07/2023.
+//
+
+import Foundation
