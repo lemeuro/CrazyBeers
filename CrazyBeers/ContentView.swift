a41cb45565e1f8e8ce2681a7d337e586342e4f99
Lem Euro <mr.lemeuro@gmail.com>
2023-07-21 16:01:50 +0100

    Initial Commit

---
+++ CrazyBeers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrazyBeers
+//
+//  Created by Lem Euro on 21/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrazyBeers/CrazyBeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyBeersApp.swift
+//  CrazyBeers
+//
+//  Created by Lem Euro on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrazyBeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
